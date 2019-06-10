817825abc9d5ae272b72853b6bb4bdb177d405bb
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2019-06-09 21:23:32 -0600

    Make MouseTracker fields volatile

--- src/gt/component/MouseTracker.java
+++ src/gt/component/MouseTracker.java
@@ -5,10 +5,10 @@ import java.util.function.Consumer;
 import gt.gamestate.UserInput;
 
 public class MouseTracker {
-    public boolean isMouseEntered = false;
-    public int mouseX;
-    public int mouseY;
-    public double wheelRotation = 0;
+    public volatile boolean isMouseEntered = false;
+    public volatile int mouseX;
+    public volatile int mouseY;
+    public volatile double wheelRotation = 0;
 
     private final Consumer<UserInput> userInputConsumer;
 
